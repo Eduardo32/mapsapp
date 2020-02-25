865ed10f43734160ca35b355e4afeb4ccc0adad2
Eduardo32 <p.eduardo.32@gmail.com>
2020-02-25 13:54:29 -0300

    Adicionando um rota fixa

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {StyleSheet} from 'react-native';
 import MapView from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions'
 
 // import { Container } from './styles';
 
-export default function App() {
-  return (
-    <MapView
-      initialRegion={{
-        latitude: -27.210671,
-        longitude: -49.63627,
-        latitudeDelta: 0.0042,
-        longitudeDelta: 0.0045,
-      }}
-      style={styles.mapView}
-    />
-  );
+export default class App extends Component {
+  state = {
+    origin: { latitude: -1.453505, longitude: -48.492556 },
+    destination: { latitude: -1.3889826, longitude: -48.4668376 },
+  }
+
+  teste = () => {
+    console.log("teste");
+  }
+
+  render() {
+    const { origin, destination } = this.state;
+    const GOOGLE_MAPS_APIKEY = 'KEY';
+
+    return (
+      <MapView
+        initialRegion={{
+          latitude: -1.453505,
+          longitude: -48.492556,
+          latitudeDelta: 0.0042,
+          longitudeDelta: 0.0045,
+        }}
+        style={styles.mapView} 
+      > 
+        <MapView.Marker 
+          coordinate={origin}
+        />
+  
+        <MapView.Marker 
+          coordinate={destination}
+        />
+
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={5}
+          strokeColor='#00a0e5'
+        />
+      </MapView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
